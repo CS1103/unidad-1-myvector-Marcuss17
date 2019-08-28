7f106c23bbabd05107037b21103154cbb2ac8f2e
Marcuss17 <46487685+Marcuss17@users.noreply.github.com>
2019-08-27 19:28:15 -0500

    Delete Rectangulo.cpp

--- Rectangulo.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "Rectangulo.h"
-
-Rectangulo::Rectangulo(int signo, float x, float y, float base, float altura) : Triangulo(signo, x, y, base, altura) {
-}
-
-float Rectangulo::calcularArea() {
-    return getB()*getA();
-}
